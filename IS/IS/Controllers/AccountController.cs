017f9e3e5005b6f85971a449cfcd9e89e075b872
Philippe CHARLES <A114SR@login.axa>
2024-01-14 12:28:20 +0100

    Commit dev enregistrement user

---
+++ IS/IS/Controllers/AccountController.cs
@@ -0,0 +1,50 @@
+﻿using IS.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IS.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<User> userManager;
+        public AccountController(UserManager<User> userManager)
+        {
+            this.userManager = userManager;
+        }
+        public IActionResult Subscription()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Subscription(SubscriptionViewModel subscriptionVM)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(subscriptionVM);
+            }
+            var result = await this.userManager.CreateAsync(new User
+            {
+                Email = subscriptionVM.Email,
+                UserName = subscriptionVM.Email,
+                FirsName = subscriptionVM.FirstName,
+                LastName = subscriptionVM.LastName,
+            }, subscriptionVM.Password);
+
+            if (result.Succeeded)
+
+                return RedirectToAction("index", "Home");
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.Code, item.Description);
+                }
+                return View(subscriptionVM);
+            }
+        }
+    }
+}
+
+
+    
---
+++ IS/IS/Models/SubscriptionViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IS.Models
+{
+    public class SubscriptionViewModel
+    {
+        [Required]
+        [Display(Name = "Adresse mail")]
+        public string Email { get; set; }
+
+        [Display(Name = "Nom de famille")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Prénom")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Mot de passe")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+
+    }
+}
